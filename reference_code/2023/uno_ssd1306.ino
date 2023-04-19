f26632502eff9cbd120cf9594ef1bece136ec359
Nick Marchuk <ndm736@users.noreply.github.com>
2023-04-19 16:36:41 -0500

    i2c oled display

---
+++ reference_code/2023/uno_ssd1306.ino
@@ -0,0 +1,40 @@
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 32  // OLED display height, in pixels
+
+// On an arduino UNO / metro mini:       A4(SDA), A5(SCL)
+#define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;  // Don't proceed, loop forever
+  }
+
+  // Clear the buffer
+  display.clearDisplay();
+}
+
+int i = 0;
+
+void loop() {
+  display.clearDisplay();
+  display.setTextSize(1);               // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);  // Draw white text
+  display.setCursor(0, 0);              // Start at top-left corner
+  display.println(F("Hello, world!"));
+  display.setCursor(0, 8);  // next line down
+  display.println(i);
+  i++;
+  display.display(); // push to the display
+}
